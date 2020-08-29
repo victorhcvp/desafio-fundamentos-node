454c8584432fa59285ca7483ca01fa579090f1ff
Victor Hugo <victorhugovergueiro@gmail.com>
2020-08-29 15:29:16 -0400

    envio do desafio

--- src/repositories/TransactionsRepository.ts
+++ src/repositories/TransactionsRepository.ts
@@ -6,23 +6,45 @@ interface Balance {
   total: number;
 }
 
+interface TransactionODT {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
+}
+
 class TransactionsRepository {
   private transactions: Transaction[];
 
+  private balance: Balance;
+
   constructor() {
     this.transactions = [];
+    this.balance = {
+      income: 0,
+      outcome: 0,
+      total: 0,
+    };
   }
 
   public all(): Transaction[] {
-    // TODO
+    return this.transactions;
   }
 
   public getBalance(): Balance {
-    // TODO
+    return this.balance;
   }
 
-  public create(): Transaction {
-    // TODO
+  public create({ title, value, type }: TransactionODT): Transaction {
+    const transaction = new Transaction({ title, value, type });
+    this.transactions.push(transaction);
+    if (type === 'income') {
+      this.balance.income += value;
+      this.balance.total += value;
+    } else {
+      this.balance.outcome += value;
+      this.balance.total -= value;
+    }
+    return transaction;
   }
 }
 
--- src/services/CreateTransactionService.ts
+++ src/services/CreateTransactionService.ts
@@ -1,6 +1,12 @@
 import TransactionsRepository from '../repositories/TransactionsRepository';
 import Transaction from '../models/Transaction';
 
+interface Request {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
+}
+
 class CreateTransactionService {
   private transactionsRepository: TransactionsRepository;
 
@@ -8,8 +14,22 @@ class CreateTransactionService {
     this.transactionsRepository = transactionsRepository;
   }
 
-  public execute(): Transaction {
-    // TODO
+  public execute({ title, value, type }: Request): Transaction {
+    if (typeof title !== 'string') {
+      throw Error('Title must be a string.');
+    }
+    if (typeof value !== 'number') {
+      throw Error('Value must be a number.');
+    }
+    if (type !== 'income' && type !== 'outcome') {
+      throw Error('Type must be income or outcome.');
+    }
+
+    const actualBalance = this.transactionsRepository.getBalance();
+    if (type === 'outcome' && actualBalance.total - value < 0) {
+      throw Error("You can't spend more than you have.");
+    }
+    return this.transactionsRepository.create({ title, value, type });
   }
 }
 
